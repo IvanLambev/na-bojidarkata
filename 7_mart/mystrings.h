8811e3e34d889b1b4751c25bd53e8fe7ef4fbd2a
ivan lambev <95970879+IvanLambev@users.noreply.github.com>
2023-03-14 04:02:05 -0400

    Delete 7_mart directory

--- 7_mart/mystrings.h
+++
@@ -1,8 +0,0 @@
-#ifndef MYSTRINGS_H
-#define MYSTRINGS_H
-
-int strlength(const char *str);
-void strconcat(char *str1, const char *str2);
-int strcompare(const char *str1, const char *str2);
-
-#endif
