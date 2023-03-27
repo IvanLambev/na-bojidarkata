9442c23848ad8445a2fe022904ef33a5fd01530c
ivan lambev <95970879+IvanLambev@users.noreply.github.com>
2023-03-27 23:39:14 +0300

    aideee trugna

--- 21_mart/safedivide.c
+++ 21_mart/safedivide.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "safeint.h"
-#include "util.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <num1> <num2>\n", argv[0]);
+        fprintf(stderr, "Usage: %s num1 num2\n", argv[0]);
         return 1;
     }
-    int num1 = safestrtoint(argv[1]);
-    int num2 = safestrtoint(argv[2]);
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
     SafeResult result = safedivide(num1, num2);
     if (result.errorflag == 1) {
-        printf("Error: Division by zero or overflow/underflow occurred\n");
+        fprintf(stderr, "Error: division by zero or overflow occurred\n");
         return 1;
     }
     printf("%d\n", result.value);
--- 21_mart/safemultiply.c
+++ 21_mart/safemultiply.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "safeint.h"
-#include "util.h"
 
 int main(int argc, char *argv[]) {
     if (argc != 3) {
-        printf("Usage: %s <num1> <num2>\n", argv[0]);
+        fprintf(stderr, "Usage: %s num1 num2\n", argv[0]);
         return 1;
     }
-    int num1 = safestrtoint(argv[1]);
-    int num2 = safestrtoint(argv[2]);
+    int num1 = atoi(argv[1]);
+    int num2 = atoi(argv[2]);
     SafeResult result = safemultiply(num1, num2);
     if (result.errorflag == 1) {
-        printf("Error: Overflow or underflow occurred\n");
+        fprintf(stderr, "Error: overflow occurred\n");
         return 1;
     }
     printf("%d\n", result.value);
